ec4b9cbc46b5996fedbc2968b58c1497bfa786e8
Malú Nakamura <malu.design10@gmail.com>
2026-07-08 20:38:45 -0500

    Actualización de la web

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-import HeroSection from "./components/HeroSection";
-import QuienesSomos from "./components/QuienesSomos";
-import ComoFunciona from "./components/ComoFunciona";
-import PlanesPreview from "./components/PlanesPreview";
-
 export default function Home() {
-  return (
-    <main>
-      <HeroSection />
-      <QuienesSomos />
-      <ComoFunciona />
-      <PlanesPreview />
-    </main>
-  );
+  return <h1>FUNCIONA 🚀</h1>;
 }
